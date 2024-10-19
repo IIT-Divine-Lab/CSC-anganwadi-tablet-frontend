4fa42df0e3516f9e4b8794a21d394ca8ff437d67
kushdeepwalia <skushdeep5@gmail.com>
2024-10-20 00:14:10 +0530

    a

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,3 +1,3 @@
-const apiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/v1/' : 'https://cscanganwadi-c6be713ed90b.herokuapp.com/api/v1';
+const apiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/v1/' : 'https://cscanganwadi-c6be713ed90b.herokuapp.com/api/v1/';
 
 export default apiUrl;
