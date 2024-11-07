451c00362ac96627d2dadf1365ea7dca621dd74f
kushdeepwalia <skushdeep5@gmail.com>
2024-11-07 17:35:41 +0530

    ui correction

--- src/QuestionStructures/Structure1-4/index.jsx
+++ src/QuestionStructures/Structure1-4/index.jsx
@@ -107,6 +107,19 @@ const Structure1to4 = ({ setActiveOption, activeOption, question }) => {
             questionImageRef.current.style.height = "auto";
          }
       }
+      else {
+         questionImageRef.current.onload = () => {
+            let img = questionImageRef.current;
+            if (img?.naturalWidth - img?.naturalHeight > 200) {
+               questionImageRef.current.style.height = "200px";
+               questionImageRef.current.style.width = "auto";
+            }
+            else {
+               questionImageRef.current.style.width = "300px";
+               questionImageRef.current.style.height = "auto";
+            }
+         }
+      }
 
    }, [question, questionImageRef])
 
